899c1eeee10eb8d27bfb0e8dff9d4d8c12ad2b41
Helder S Ribeiro <hsribei.pub@gmail.com>
2017-12-23 11:08:47 -0300

    Fetch data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import "./style.css";
 
+function fetchAndExecutewithJson(
+    url: string,
+    jsonConsumer: (json: object) => void
+) {
+    fetch(url).then(response => {
+        response.json().then(jsonConsumer);
+    });
+}
+
+function appendJsonPre(json: object): void {
+    const pre = document.createElement("pre");
+    pre.innerHTML = JSON.stringify(json, null, 2);
+    document.getElementById("d3").appendChild(pre);
+}
+
 document.addEventListener("DOMContentLoaded", event => {
-  console.log("hey!");
+    const url =
+        "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+    fetchAndExecutewithJson(url, appendJsonPre);
 });
